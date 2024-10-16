2792cccc8f316280cf23351cbb1e7062f1aa7b49
RahmadahyaWeb <thelastpc24@gmail.com>
2024-10-16 10:14:38 +0700

    test

--- app/Http/Controllers/ReportDKSController.php
+++ app/Http/Controllers/ReportDKSController.php
@@ -28,14 +28,12 @@ class ReportDKSController extends Controller
     {
         $this->guard();
 
-        return 'Sedang Perbaikan';
+        // return 'Sedang Perbaikan';
 
         $request->validate([
             'fromDate'  => 'required',
             'toDate'    => 'required',
         ]);
-
-        ini_set('max_execution_time',3600);
 
         return Excel::download(new DksExport($request->fromDate, $request->toDate), 'dks.xlsx');
     }
