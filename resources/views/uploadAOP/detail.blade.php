b986cc4d567ece1c0ae247b10df50fbf5fc75fb4
RahmadahyaWeb <thelastpc24@gmail.com>
2024-10-23 15:33:27 +0700

    upload non aop selesai

--- resources/views/uploadAOP/detail.blade.php
+++ resources/views/uploadAOP/detail.blade.php
@@ -8,4 +8,21 @@
 
 @push('scripts')
     @livewireScripts()
+    <script>
+        Livewire.on('fakturPajakUpdate', () => {
+            $('#editFakturPajakModal').modal('hide');
+        })
+
+        Livewire.on('openModal', () => {
+            $('#editFakturPajakModal').modal('show');
+        })
+
+        Livewire.on('programSaved', () => {
+            $('#createProgramModal').modal('hide');
+        })
+
+        Livewire.on('openModalProgram', () => {
+            $('#createProgramModal').modal('show');
+        })
+    </script>
 @endpush
