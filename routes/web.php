e3dd817115d9138254bcf0115aaa31d3b7d2487a
RahmadahyaWeb <thelastpc24@gmail.com>
2024-10-29 15:38:17 +0700

    penambahan

--- routes/web.php
+++ routes/web.php
@@ -68,10 +68,10 @@ Route::middleware(['auth', 'check.online', 'auth.session'])->group(function () {
     Route::get('/non-aop/detail/{invoiceNon}', [NonAopController::class, 'detail'])->name('non-aop.detail');
 
     // AOP GR
-    Route::get('/aop-gr', [AopReceiptController::class, 'index'])->name('aop-gr.index');
+    Route::get('/gr/aop', [AopReceiptController::class, 'index'])->name('aop-gr.index');
 
     // AOP GR DETAIL
-    Route::get('/aop-gr/detail/{invoiceAop}/{spb}', [AopReceiptController::class, 'detail'])->name('aop-gr.detail');
+    Route::get('/gr/aop/{invoiceAop}/{spb}', [AopReceiptController::class, 'detail'])->name('aop-gr.detail');
 
     // LOGOUT
     Route::get('logout', [AuthController::class, 'logout'])->name('logout');
